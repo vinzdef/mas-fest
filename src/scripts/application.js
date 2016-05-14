09ca0dda51758778355ea08c2512c6983a7f43e5
Vincent <ghzmdr@gmail.com>
2016-05-14 15:22:43 +0200

    Style changes

--- src/scripts/application.js
+++ src/scripts/application.js
@@ -3,13 +3,13 @@ export default class Application {
 		this.avatar = document.querySelector('.Avatar')
 		//this.avatarWidth =
 		window.addEventListener('mousemove', this.onMove.bind(this))
+		window.addEventListener('touchmove', this.onMove.bind(this))
 	}
 
 	onMove(e) {
-		const x = e.clientX
-		const y = e.clientY
+		const x = typeof(e.clientX) === 'undefined' ? e.touches[0].screenX : e.clientX
+		const y = typeof(e.clientY) === 'undefined' ? e.touches[0].screenY : e.clientY
 
-		this.avatar.style.top = x + 'px'
-		this.avatar.style.left = y + 'px'
+		this.avatar.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0.001px)`
 	}
 }
